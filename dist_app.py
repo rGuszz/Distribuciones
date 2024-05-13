c493594cac8fcad82ae855b9ab841acc9a1afda4
rGuszz <gusrodriguezayala@gmail.com>
2024-05-12 19:48:43 -0600

    Update dist_app.py

--- dist_app.py
+++ dist_app.py
@@ -10,7 +10,7 @@ with t:
     st.write("Gráfica de la función de distribución")
     fc.normal(media,sigma)
 
-    st.write(f"La esperanza de la normal es $\mathbb{E} (X) = {media}$")
+    st.write(f"La esperanza de la normal es $E(X) = {media}$")
 
  
     
