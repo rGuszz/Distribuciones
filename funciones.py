cb251eecd7fedddb867b46c20d78d102fc6cf30f
rGuszz <gusrodriguezayala@gmail.com>
2024-05-12 19:43:29 -0600

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -29,7 +29,7 @@ def normal(media, sigma):
                              legendgroup="Funcion",
                              name="Función de densidad",
                             showlegend=False))
-    fig.update_layout(template=None, dragmode = "pan", autosize=False, width=500, height=250)
+    fig.update_layout(template=None, dragmode = "pan", autosize=False, width=500, height=350)
     if media == 0 and sigma == 1:
         fig.update_layout(xaxis=dict(zeroline=True, zerolinecolor='black'), yaxis=dict(zeroline=True, zerolinecolor='black'))
         fig.update_xaxes(range=[-4,4])
