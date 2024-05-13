d3adeb6c56f380702734931b2eee5c3e1fc969d7
rGuszz <gusrodriguezayala@gmail.com>
2024-05-12 20:02:21 -0600

    Update dist_app.py

--- dist_app.py
+++ dist_app.py
@@ -11,7 +11,6 @@ with t:
     fc.normal(media,sigma)
 
     st.write(f"Si X es la varibale aleatoria tal que $X$ ~ $N({media},{sigma})$")
-    st.latex("\usepackage{mathbb}")
     st.latex("Parámetros: $\mu \in \R, \sigma > 0$")
     st.write(f"La esperanza es $E(X) = {media}$")
     st.write(f"La varianza es $Var(X) = {sigma}$")
