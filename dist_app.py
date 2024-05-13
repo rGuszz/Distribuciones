43f035e938a8326668ad7d7b090c8123899c186b
rGuszz <gusrodriguezayala@gmail.com>
2024-05-12 20:07:43 -0600

    Update dist_app.py

--- dist_app.py
+++ dist_app.py
@@ -6,7 +6,7 @@ t = st.container()
 with t:
     st.header("Distribución Normal")
     st.write("$\large Parámetros:$ $\mu \in \R, \sigma > 0$")
-     st.write("$\large Notación:$ $X$ ~ $N(\mu,\sigma ^2)$")
+    st.write("$\large Notación:$ $X$ ~ $N(\mu,\sigma ^2)$")
     media = st.number_input("Introduce la media", min_value=-10000.00, max_value=10000.00, value=0.00, step=1.00)
     sigma = st.number_input("Introduce la varianza", min_value=0.1, max_value=10000.00, value=1.00, step=1.00)
     st.write("Gráfica de la función de distribución")
