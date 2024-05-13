8c28119622e064dcfe65232896f9d2a171c34c97
rGuszz <gusrodriguezayala@gmail.com>
2024-05-12 19:48:09 -0600

    Update dist_app.py

--- dist_app.py
+++ dist_app.py
@@ -5,7 +5,12 @@ t = st.container()
 
 with t:
     st.header("Normal")
-    media = st.number_input("Introduce la media", min_value=-10000.00, max_value=10000.00, value=0.00, step=0.01)
-    sigma = st.number_input("Introduce la varianza", min_value=0.1, max_value=10000.00, value=1.00, step=0.01)
+    media = st.number_input("Introduce la media", min_value=-10000.00, max_value=10000.00, value=0.00, step=1.00)
+    sigma = st.number_input("Introduce la varianza", min_value=0.1, max_value=10000.00, value=1.00, step=1.00)
+    st.write("Gráfica de la función de distribución")
     fc.normal(media,sigma)
+
+    st.write(f"La esperanza de la normal es $\mathbb{E} (X) = {media}$")
+
+ 
     
